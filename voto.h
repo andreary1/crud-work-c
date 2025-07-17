abbc48207eb98ff7808f257d7d0d4f5bb4a03bf7
marinagfontenele <marinagomesfontenele2007@gmail.com>
2025-07-17 11:41:42 -0300

    Add files via upload

--- voto.h
+++ voto.h
@@ -1,8 +1,29 @@
-//
-// Created by andre on 07/07/2025.
-//
+#pragma once
 
-#ifndef VOTO_H
-#define VOTO_H
+struct voto {
+    ano;
+    codigo_UF;
+    numero_candidato;
+    data_hora[10];
+};
 
-#endif //VOTO_H
+enum opcao_voto {
+    INSERIR_VOTO = 1;
+    MOSTRAR_VOTOS_CANDIDATOS = 2;
+    MOSTRAR_TUDO_VOTOS = 3;
+    SAIR_VOTOS = 4;
+}
+
+typedef enum opcao_voto opcao_voto;
+
+typedef struct voto voto;
+
+void menuVoto (voto *votos[], int *num_votos); //FEITO 
+int carregarVotos (voto *votos[], int num_votos); // FEITO
+void liberarVotos (voto *votos[], int num_votos); // FEITO
+void inserirVoto (voto *votos[], int *num_votos);
+void mostrarVotosCandidato (voto *votos[], int *num_votos); 
+void mostrarTudoVotos (voto *votos[], int *num_votos);
+int verificarCandidatoVoto (voto *votos[], int num_votos); // FEITO // verifica se o candidato existe
+int verificarDataHora (voto *votos[], int num_votos);  // FEITO // verifica se já não tiveram votos nesse mesmo momento (devido PK)
+// void verificarTitulo (voto *votos[], int *num_votos);  // FEITO // VER SE PRECISA MESMO
